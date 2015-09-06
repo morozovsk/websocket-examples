d810bfbc471d5f44ea917f8e5f8f96503c6d61c1
morozovsk <morozovsk@gmail.com>
2015-09-06 17:02:04 +0300

    fix path

--- game/server/index.php
+++ game/server/index.php
@@ -15,7 +15,7 @@ $config = array(
     'timer' => 0.1
 );
 
-require_once __DIR__ . '/../../../../../autoload.php';
+require_once __DIR__ . '/../../../../autoload.php';
 
 $WebsocketServer = new morozovsk\websocket\Server($config);
 call_user_func(array($WebsocketServer, $argv[1]));
